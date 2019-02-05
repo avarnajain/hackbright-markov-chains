69b9acaaa7b3245907aa6d8aae7179b789fc78fe
vagrant <vagrant@vagrant.vm>
2019-02-05 20:04:13 +0000

    dictionary comprehension splitting values into single characters, of only one word. List is not appending

--- markov_further_study.py
+++ markov_further_study.py
@@ -6,51 +6,15 @@ import sys
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
-
-    Takes a string that is a file path, opens the file, and turns
-    the file's contents as one string of text.
     """
 
     file_object = open(file_path)
-    #file_object.read() 
-    #print (file_object)
-
-    # join_string = ""
-
-    # for line in file_object:
-    #     line = line.rstrip() 
-    #     join_string = join_string + line + " "
-
-
 
     return file_object.read()
-    #print (join_string)
-    #return join_string
 
 
 def make_chains(text_string,n=2):
     """Take input text as string; return dictionary of Markov chains.
-
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
-
-    For example:
-
-        >>> chains = make_chains("hi there mary hi there juanita")
-
-    Each bigram (except the last) will be a key in chains:
-
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-    Each item in chains is a list of all possible following words:
-
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-        
-        >>> chains[('there','juanita')]
-        [None]
     """
 
     chains = {}
@@ -72,25 +36,26 @@ def make_chains(text_string,n=2):
             else: 
                 chains[tuple(string_list[i:i+n])] = [string_list[i+n]]
 
+    return chains
+    
+def make_chain2(text_string,n=2): 
+    """atttempt make_chains with dictionary comprehension"""
 
     # for keys in chains:
     #     chains[keys] = list(set(chains[keys]))
     #print('dictionary', chains)
             
     # dictionary containing key:value for every key where the key is a tuple(ngram) and value 
-    # is a list of next words  
-def make_chain2(text_string,n=2): 
+    # is a list of next words 
 
-    # your code goes here
     string_list = text_string.split() + ['my_very_special_last_word']
-    print ({
-        tuple(string_list[i:i+n]):list(string_list[i+n]) 
-        for i in range(0, len(string_list-n))})
-
-    return None
-
-
-
+    tuple_dict = {
+        tuple(string_list[i:i+n]):list(string_list[j+n])
+        for i, j
+        in zip(range(0, len(string_list)-n), range(0, len(string_list)-n)) 
+        }
+    print(tuple_dict)
+    return tuple_dict
 
 def make_text(chains):
     """Return text from chains."""
